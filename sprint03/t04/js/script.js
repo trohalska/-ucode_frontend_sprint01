59ada299d05a8c695676e57ae184cb5d18ac332f
Tetiana Rohalska <55732200+trohalska@users.noreply.github.com>
2020-08-10 10:23:32 +0300

    sprint 03 t00-t06

---
+++ sprint03/t04/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function concat(s1, s2) {
+  oneArg.count = 0;
+
+  function oneArg() {
+    let s2 = prompt('Enter second string: ');
+
+    oneArg.count++;
+    return s1 + ' ' + s2;
+  }
+
+  if (s2 === undefined)
+    return oneArg;
+  else
+    return s1 + ' ' + s2;
+
+}
